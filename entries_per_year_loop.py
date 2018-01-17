f29a36ab485c73221fb7c0dbf730d8ed0dcebce8
Ninalina <n.p.hartter@students.uu.nl>
2018-01-17 11:21:02 +0100

    adds the code to get the number of entries (birthdate matching file name) for each year

---
+++ entries_per_year_loop.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import logging, csv, re, sys, codecs
+
+floatre = re.compile("^\d+\.\d+$")
+intre = re.compile("^\d+$")
+
+def read_header(file="h.txt"):
+    header=[]
+    for line in open(file):
+        header.append(line.strip())
+    logging.info("%d lines in header", len(header))
+    return header
+
+def process_csv(file, header):
+    out=[]
+    stdin = file == "-"
+    fd = sys.stdin if stdin else codecs.open(file, 'r', 'UTF-8')
+    reader = csv.reader(fd)
+    for nr, row in enumerate(reader):
+        logging.debug("%d fields in line %d", len(row), nr)
+        d = dict()
+        out.append(d)
+        for i, field in enumerate(row):
+            if field != "NULL":
+                if floatre.match(field):
+                    d[header[i]] = float(field)
+                elif intre.match(field):
+                    d[header[i]] = int(field)
+                else:
+                    d[header[i]] = field
+    if not stdin:
+        fd.close()
+    return out
+entries_per_year = 0
+for number in ("years\\1500"):
+    if __name__ == "__main__":
+        header = read_header("h.txt")
+        out = process_csv("years\\1500", header)
+        for person in out:    
+            if person['birthDate'][0] == '1':
+                    person['birthDate'] = str(person['birthDate'][0]) + str(person['birthDate'][1]) + str(person['birthDate'][2]) + str(person['birthDate'][3])                   
+            else:
+                    person['birthDate'] = str(person['birthDate'][1]) + str(person['birthDate'][2]) + str(person['birthDate'][3]) + str(person['birthDate'][4])
+            if int(person["birthDate"]) == 1500:
+                    entries_per_year += 1

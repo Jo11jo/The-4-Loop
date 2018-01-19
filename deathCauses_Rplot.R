de2cb3627d71159de9c8d308deea35995ce30e22
Ninalina <n.p.hartter@students.uu.nl>
2018-01-19 17:06:31 +0100

    new graphs

--- deathCauses_Rplot.R
+++ deathCauses_Rplot.R
@@ -7,4 +7,4 @@ common_deathCauses <- read.csv('common_deathCauses.csv')
 # This makes a bar graph of the most common death causes in the years 1820 until 1920
 ggplot(common_deathCauses, aes(x = reorder(cause, deaths), y = deaths), position = position_stack(reverse = TRUE)) + coord_flip() + theme(legend.position = "top") +
   geom_bar(stat = "identity", fill = "red") +
-  labs(x = "Death causes", y = "Deaths")
+  labs(x = "Death Causes", y = "Deaths")

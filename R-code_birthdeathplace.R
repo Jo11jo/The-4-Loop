0c9e8cf060abd60980e31fa496f0c5192dd03953
Ninalina <n.p.hartter@students.uu.nl>
2018-01-19 17:07:43 +0100

    new graphs

--- R-code_birthdeathplace.R
+++ R-code_birthdeathplace.R
@@ -1,5 +1,5 @@
 library("tidyverse")
-
+library("ggplot2")
 bdplace <- read.csv("birth_death_places.csv")
 
 # Creates a column with the percentage of people that were born and died in the same place
@@ -7,5 +7,9 @@ percentageincluded <- bdplace %>%
   mutate(percentage = birth_death_same/(birth_death_same + birth_death_notsame)*100)
 # Plots the development of percentages over time
 ggplot()+
-  geom_line(data = percentageincluded, mapping = aes(x = year, y = percentage), colour = "red")
-
+  geom_line(data = percentageincluded, mapping = aes(x = year, y = percentage), colour = "red") + labs(x = "Year", y = "Percentage") +
+  theme(plot.margin=unit(c(0.5,1,0.5,0.5),"cm"))
+# Plots the development of percentages over time as a scatter plot
+ggplot()+
+  geom_point(data = percentageincluded, mapping = aes(x = year, y = percentage), colour = "red") + labs(x = "Year", y = "Percentage") +
+  theme(plot.margin=unit(c(0.5,1,0.5,0.5),"cm"))
